acf0dfc41e4ed0d72357eb6c8b3808f276c04487
Bap <23110020@student.hcmute.edu.vn>
2026-05-30 21:10:39 +0700

    Revert "Implement fragment-based navigation and add core app screens"
    This reverts commit 0a4e5bcbf67129a2a8cc8260e8ea2e8ed50b0c41.

---
+++ app/src/main/java/com/hcmute/bookreadingapp/AudioPlayerActivity.java
@@ -0,0 +1,13 @@
+package com.hcmute.bookreadingapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AudioPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_player);
+    }
+}
---
+++ app/src/main/java/com/hcmute/bookreadingapp/PodCourseDetailActivity.java
@@ -0,0 +1,23 @@
+package com.hcmute.bookreadingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PodCourseDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_podcourse_detail);
+
+        LinearLayout lesson1 = findViewById(R.id.layout_lesson_1);
+        if (lesson1 != null) {
+            lesson1.setOnClickListener(v -> {
+                Intent intent = new Intent(PodCourseDetailActivity.this, AudioPlayerActivity.class);
+                startActivity(intent);
+            });
+        }
+    }
+}
